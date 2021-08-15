fd461e41de0b316c3dacf55f2bf71a909421a5bc
Jonyaey <54909917+JhonnyEkYam@users.noreply.github.com>
2021-08-14 19:14:49 -0500

    login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,14 @@
 import { Response, Request } from "express";
 
 import { UserQueries } from "../queries/user.queries";
+import jwt from "jsonwebtoken";
 
 class userController{
 
-    public async getUser(req:Request, res:Response){
+    public async getUsers(req:Request, res:Response){
 
         const request = req.body;
-        const user = await UserQueries.getUser(request);
+        const user = await UserQueries.getUsers(request);
         if(user.ok){
             return res.json({data:user});
 
@@ -16,6 +17,29 @@ class userController{
 
         }
 
+    }
+
+    public async login(req: Request, res: Response){
+        const userData = req.body;
+        if(userData.user && userData.password){
+            const user = await UserQueries.getUser(userData);
+
+            // console.log(user)
+            const token = jwt.sign({
+                data: {
+                    role: "user",
+                    user: user,
+                }
+            }, process.env.ENCRYPT_KEY)
+            // userInfo.token = token;
+            // this.msg = "ok";
+            // this.ok = true;
+            
+
+            return res.json({data:user})
+        }else{
+            return res.status(400).json({ok:false});
+        }
     }
 }
 
--- src/queries/user.queries.ts
+++ src/queries/user.queries.ts
@@ -1,13 +1,27 @@
 import { UserModel } from '../models/user.model';
 
 class userQueries {
-    public async getUser(data) {
+    public async getUsers(data) {
         try {
             const query = await UserModel.findAll();
             return { ok: true, data: query }
         } catch (e) {
             console.log(e);
             return { ok: false, data: null };
+        }
+    }
+
+    public async getUser(user){
+        try {
+            const query = await UserModel.findOne({where: {user: user.user}});
+            if(query){
+                return {ok: true, data: query};
+            }else {
+                return {ok: false, data: null};
+            }
+        } catch (e) {
+            console.log(e);
+            return {ok: false, data: null};
         }
     }
 }
